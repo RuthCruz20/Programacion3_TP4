cab9eb989b5d2d010cf1495732d7ae9982f167fa
Ruth Cruz <shuth.1417@gmail.com>
2023-10-04 00:42:21 -0300

    Corrección ejercicio 6

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -8,11 +8,34 @@ class Producto:
 
 class Inventario:
 
-    def __init__(self, Producto):
-        self.producto = Producto
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.productos = []
 
-    def getProducto(self):
-        print(f"Producto, {self.producto}!")
+    def add_producto(self, producto):
+        self.productos.append(producto)
+    def delete_producto(self, producto):
+        self.productos.remove(producto)
+    def get_inventario(self):
+        print(f"Inventario, {self.nombre}!")
+        i=1
+        for producto in self.productos:
+            print(f"Posición: ", i)
+            print(f"Producto:{producto.nombre}")
+            i=i+1
 def main():
-    producto = Producto("Test")
-    inventario = Inventario(producto)
+    producto1 = Producto("PC")
+    producto2 = Producto("Impresora")
+    producto3 = Producto("TV")
+
+    inventario = Inventario("Inventario JNC")
+    print("Inventario Inicial")
+    inventario.get_inventario()
+    inventario.add_producto(producto1)
+    inventario.add_producto(producto2)
+    inventario.add_producto(producto3)
+    print("Carga inicial")
+    inventario.get_inventario()
+    print("Eliminar un elemento")
+    inventario.delete_producto(producto2)
+    inventario.get_inventario()
